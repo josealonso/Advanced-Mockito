4c67d4fc4a193607c34a2c1233e59c094226c9a8
josealonso <jose.alonso.programmer@gmail.com>
2024-08-31 01:30:08 +0200

    First approach to creating mocks with Mockito.

--- src/test/java/de/rieckpil/courses/mocking/RegistrationServiceV1Test.java
+++ src/test/java/de/rieckpil/courses/mocking/RegistrationServiceV1Test.java
@@ -1,7 +1,6 @@
 package de.rieckpil.courses.mocking;
 
 import de.rieckpil.courses.BannedUsersClient;
-import de.rieckpil.courses.JpaUserRepository;
 import de.rieckpil.courses.RegistrationService;
 import de.rieckpil.courses.UserRepository;
 import org.junit.jupiter.api.Test;
@@ -13,8 +12,20 @@ class RegistrationServiceV1Test {
   private BannedUsersClient bannedUsersClient = Mockito.mock(BannedUsersClient.class);
   private RegistrationService cut = new RegistrationService(userRepository, bannedUsersClient);
 
+  /*
+  How Mockito works: https://medium.com/@gorali/how-mockito-works-7d3a2c77da71
+  userRepository is not an instance of UserRepository, and it only exists
+  in execution time.
+  */
   @Test
   void shouldRegisterUnknownUser() {
+    System.out.println("============== Mocking UserRepository ==============");
+    System.out.println(userRepository.toString());
+    System.out.println(userRepository.getClass());
+
+    System.out.println("============== Mocking BannedUsersClient ==============");
+    System.out.println(bannedUsersClient.toString());
+    System.out.println(bannedUsersClient.getClass());
   }
 
 }
