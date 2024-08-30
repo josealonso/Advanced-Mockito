5c88fd7cc4f4a5b6c7f813155ec8acfb265b3373
josealonso <jose.alonso.programmer@gmail.com>
2024-08-31 01:49:39 +0200

    Third and most common approach: using the MockitoExtension.

--- src/test/java/de/rieckpil/courses/mocking/RegistrationServiceV3Test.java
+++ src/test/java/de/rieckpil/courses/mocking/RegistrationServiceV3Test.java
@@ -9,7 +9,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-// @RunWith(MockitoJUnitRunner.class)
+// This annotation calls MockitoAnnotations.openMocks() behind the scenes
 @ExtendWith(MockitoExtension.class)
 class RegistrationServiceV3Test {
 
@@ -24,5 +24,9 @@ class RegistrationServiceV3Test {
 
   @Test
   void shouldRegisterUnknownUser() {
+    System.out.println(userRepository.getClass());
+    System.out.println(bannedUsersClient.getClass());
+
+    System.out.println(cut.toString());
   }
 }
