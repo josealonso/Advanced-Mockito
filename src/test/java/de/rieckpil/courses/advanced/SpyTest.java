c4afc88fe9d485eade8ae3358ee1e9f004c60741
josealonso <jose.alonso.programmer@gmail.com>
2024-09-19 17:49:13 +0200

    @Mock versus @Spy.

--- src/test/java/de/rieckpil/courses/advanced/SpyTest.java
+++ src/test/java/de/rieckpil/courses/advanced/SpyTest.java
@@ -13,14 +13,30 @@ import java.util.List;
 @ExtendWith(MockitoExtension.class)
 class SpyTest {
 
-  @Spy
+  @Spy // A spy is not a mock
   private BannedUsersClient bannedUsersClient;
 
+  // A spy invokes the actual real method, unless the method is overwritten. Spies
+  // are rarely used.
   @Test
   void spies() {
+
+    Mockito.when(bannedUsersClient.amountOfBannedAccounts()).thenReturn(400);
+
+    System.out.println(bannedUsersClient.amountOfBannedAccounts()); // 400
+    System.out.println(bannedUsersClient.banRate()); // 42.42
   }
 
   @Test
   void spiesGotcha() {
+    List spiedList = Mockito.spy(ArrayList.class);
+
+    // Mockito.when(spiedList.get(0)).thenReturn("spy"); // Error
+
+    Mockito.doReturn("spy").when(spiedList.get(0));
+
+    System.out.println(spiedList.get(0)); // "spy"
+    System.out.println(spiedList.get(1)); // IndexOutOfBoundsException
+
   }
 }
